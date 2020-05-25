bf3aec51641b8225d6ae9e2e82e9ac2befb3e45e
mrv07 <venkatesh.nowwin@gmail.com>
2020-05-25 11:27:24 +0530

    first commit

---
+++ js/progress-circle/custom.js
@@ -0,0 +1,53 @@
+	(function($) {
+		"use strict";
+	
+	
+
+	$(function () {
+	$('.percent').percentageLoader({
+		valElement: 'p',
+		strokeWidth:5,
+		bgColor: 'rgba(255, 255, 255, 0.1)',
+		ringColor: '#fce206',
+		textColor: '#fff',
+		fontSize: '24px',
+		fontWeight: 'normal'
+	});
+
+	});
+
+
+	$(function () {
+	$('.percent-2').percentageLoader({
+		valElement: 'p',
+		strokeWidth:5,
+		bgColor: 'rgba(255, 255, 255, 0.1)',
+		ringColor: '#fce206',
+		textColor: '#fff',
+		fontSize: '24px',
+		fontWeight: 'normal'
+	});
+
+	});
+
+
+	$(function () {
+	$('.percent-3').percentageLoader({
+		valElement: 'p',
+		strokeWidth:5,
+		bgColor: 'rgba(0, 0, 0, 0.4)',
+		ringColor: '#000',
+		textColor: '#000',
+		fontSize: '24px',
+		fontWeight: 'normal'
+	});
+
+	});
+
+
+
+
+	
+
+	
+	})(jQuery);
